7aed61aa5eaa47ac01066a7bf1bfc40417a0a3a9
Mihail_Gah <mihail_gah@epam.com>
2020-09-26 10:35:53 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$(function() {
+  $(".banner-section__slider").slick({
+    dots: true,
+    prevArrow: `
+      <button class="banner-section__slider-btn banner-section__slider-btnprev">
+        <img src="images/arrow-left.svg" alt="" />
+      </button>
+    `,
+    nextArrow: `
+      <button class="banner-section__slider-btn banner-section__slider-btnnext">
+        <img src="images/arrow-right.svg" alt="" />
+      </button>
+    `
+  });
+
+  $(".tab").on("click", function(evt) {
+    evt.preventDefault();
+
+    // this === текущий .tab, по которому кликнули
+    $($(this).siblings()).removeClass("tab--active");
+    $($(this).parent().siblings().find(".tabs-content")).removeClass("tabs-content--active");
+
+    $(this).addClass("tab--active");
+    $($(this).attr("href")).addClass("tabs-content--active");
+  });
+
+  $(".product-item__fav").on("click", function(evt) {
+    evt.preventDefault();
+    $(this).toggleClass("product-item__fav--active");
+  });
+
+  $(".product-slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: `
+      <button class="product-slider__slider-btn product-slider__slider-btnprev">
+        <img src="images/arrow-black-left.svg" alt="" />
+      </button>
+    `,
+    nextArrow: `
+      <button class="product-slider__slider-btn product-slider__slider-btnnext">
+        <img src="images/arrow-black-right.svg" alt="" />
+      </button>
+    `
+  });
+});
